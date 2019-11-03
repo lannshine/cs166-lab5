ea30df01d4e1bb5774a60dc1b4596fffa4dc4a09
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2019-11-02 19:51:58 -0700

    Reset Q5

--- EmbeddedSQL.java
+++ EmbeddedSQL.java
@@ -318,19 +318,8 @@ public class EmbeddedSQL {
 
    public static void Query5(EmbeddedSQL esql){
       // Your code goes here.
-      try{
-         String query = "SELECT s.sname, COUNT(c.pid) FROM suppliers s, parts p, catalog c WHERE s.sid=c.sid AND p.pid=c.pid GROUP BY s.sname";
-         
-		 int rowCount = esql.executeQuery(query);
-		 if (rowCount == 0){
-			 System.out.println ("No results.");
-		 }
-		 else {
-			 System.out.println ("Total row(s): " + rowCount);
-		 }		 
-      }catch(Exception e){
-         System.err.println (e.getMessage());
-      }
+      // ...
+      // ...
    }//end Query5
 
    public static void Query6(EmbeddedSQL esql){
